b5e2d1750ce39c44e669dafe59cb923f89b2f1e3
naru-test-admin <ggobugi3@gmail.com>
2023-03-13 21:36:35 +0900

    230313_spring-boot_@Autowired error_(UserDAO)

--- anabada/src/main/java/com/anabada/controller/login/LoginController.java
+++ anabada/src/main/java/com/anabada/controller/login/LoginController.java
@@ -1,5 +1,23 @@
 package com.anabada.controller.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.anabada.service.login.LoginService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class LoginController {
 
+	@Autowired
+	private LoginService service;
+	
+	@GetMapping("/")
+	public String login() {
+		service.allUser();
+		return "/checkPwForm";
+	}
 }
--- anabada/src/main/java/com/anabada/domain/UserDTO.java
+++ anabada/src/main/java/com/anabada/domain/UserDTO.java
@@ -1,5 +1,10 @@
 package com.anabada.domain;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,19 +12,55 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO implements UserDetails {
 	
-	String user_email;
-	String user_nick;
-	String user_pwd;
-	String user_phone;
-	String user_addr;
-	int user_level;
-	int user_trade;
-	int user_penalty;
-	int user_account;
-	String user_role;
-	String user_nation;
-	String user_date;
 	
+	
+	private String user_email;
+	private String user_nick;
+	private String user_pwd;
+	private String user_phone;
+	private String user_addr;
+	private int user_level;
+	private int user_trade;
+	private int user_penalty;
+	private boolean user_account;
+	private String user_role;
+	private String user_nation;
+	private String user_date;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.user_pwd;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.user_email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return this.user_account;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
