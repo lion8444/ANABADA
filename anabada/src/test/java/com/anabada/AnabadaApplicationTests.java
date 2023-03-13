2b3e79988a8d507bbbb9ae96a15442143fba6946
naru-test-admin <ggobugi3@gmail.com>
2023-03-13 09:25:55 +0900

    spring

---
+++ anabada/src/test/java/com/anabada/AnabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
