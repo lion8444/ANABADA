781e7d7069ce0583318f5dbb122713e10d68ea7b
klaus hyeon <klaushyeon@gmail.com>
2023-03-24 10:16:07 +0900

    23.03.24 dashboard table

--- anabada/src/main/java/com/anabada/service/admin/AdminService.java
+++ anabada/src/main/java/com/anabada/service/admin/AdminService.java
@@ -1,10 +1,14 @@
 package com.anabada.service.admin;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.anabada.domain.Admin_board;
+import com.anabada.domain.Auction_detail;
+import com.anabada.domain.Rental_detail;
+import com.anabada.domain.Used_detail;
 import com.anabada.domain.UserDTO;
 
 @Service
@@ -17,5 +21,13 @@ public interface AdminService {
 	String statusChange(String select, Integer index);
 
 	UserDTO usermodify(String selectn, String selects, Integer index);
+
+	int[][] getdata(int[] monthnum);
+
+//	ArrayList<Auction_detail> getauctiondetail();
+//
+//	ArrayList<Rental_detail> getrentaldetail();
+//
+//	ArrayList<Used_detail> getuseddetail();
 
 }
